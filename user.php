201edc1a701b2171a75b0fca817f671d90d1d35a
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-18 17:25:44 +0600

    title is updated

--- user.php
+++ user.php
@@ -26,7 +26,7 @@ if(isset($_POST['submit'])) {
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>CRUD Application</title>
+    <title>CRUD Application - User Sign up</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
